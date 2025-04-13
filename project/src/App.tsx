75fcbf48dc189c6b18c237ad0d97cea5a4f2f14a
Kritam Bhattarai <83293645+hackerkritam@users.noreply.github.com>
2025-04-13 22:04:26 +0545

    sdfsdf

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -209,7 +209,7 @@ function App() {
       state.speed
     );
 
-    // Visualize t
+    // Visualize 
     for (const cell of path) {
       if (cell.type !== 'start' && cell.type !== 'end') {
         setState(prev => {
