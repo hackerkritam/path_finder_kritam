acfba98e17a8cdd36674b507a07e4cc285219610
Kritam Bhattarai <83293645+hackerkritam@users.noreply.github.com>
2025-04-14 15:38:01 +0545

    sdfsdf

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -209,7 +209,7 @@ function App() {
       state.speed
     );
 
-    // ssdfsfsdfsdf
+    // ssdfsfsdfsdfsdfsdf
     for (const cell of path) {
       if (cell.type !== 'start' && cell.type !== 'end') {
         setState(prev => {
